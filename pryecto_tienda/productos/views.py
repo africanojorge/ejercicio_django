5fda33ab69e6e585514eac748d9a8004b9444266
africanojorge <africanojorge@gmail.com>
2024-10-25 22:45:28 -0300

    primer commit

---
+++ pryecto_tienda/productos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Producto
+
+# Create your views here.
+
+def index(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/index.html', {'productos': productos})
+     
